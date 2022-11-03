0f63abd9e9a8f72f57659da6a587c9d0e1011310
sitinurkaffah <92791203+sitinurkaffah@users.noreply.github.com>
2022-11-03 10:23:10 +0700

    Update api.php

--- restapi/routes/api.php
+++ restapi/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 # method get
 Route::get('/animals', [AnimalController::class, 'index']);
 
@@ -34,3 +31,19 @@ Route::put('/animals/{id}', [AnimalController::class, 'update']);
 
 # method delete
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+# get all resource students
+# method get
+Route::get('/students', [StudentController::class, 'index']);
+
+# menambahkan resource student
+# method post
+Route::post('/students', [StudentController::class, 'store']);
+
+# menambahkan resource student
+# method put
+Route::put('/students/{id}' , [StudentController::class, 'update']);
+
+# menambahkan resource student
+# method delete
+Route::delete('/students/{id}' , [StudentController::class, 'destroy']);
